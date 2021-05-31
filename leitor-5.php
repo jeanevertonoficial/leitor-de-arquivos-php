e3cd4d340c30974b9a302807c35b1ed0ca1cceed
Jean Everton <jeanever39@gmail.com>
2021-05-31 18:12:46 -0300

    Pausa no curso

--- leitor-5.php
+++ leitor-5.php
@@ -1,10 +1,12 @@
 <?php 
 
 
-$arquivo =  fopen("escritor-cursos.txt", "a"); // criar um aquivo com o nome curso-php.txt , A ele coloca o cursor no final do arquivo e continua escrevendo.
+$arquivo =  fopen("escritor.txt", "a"); // criar um aquivo com o nome curso-php.txt , A ele coloca o cursor no final do arquivo e continua escrevendo.
 
 
-$curso = "Curso de estudo em php na alura por jean everton" .PHP_EOL;
+$curso = "PHP_INT_MIN (int), O menor inteiro suportado nesta compilação do PHP. Geralmente int(-2147483648) em sistemas 32 bits e int(-9223372036854775808) em sistemas 64 bit. Normalmente PHP_INT_MIN === ~PHP_INT_MAX." .PHP_EOL.
+"PHP_EOL (string), O símbolo correto de 'Fim de linha' para esta plataforma." .PHP_EOL.
+"PHP_INT_MAX (int), O maior inteiro suportado nesta compilação do PHP. Geralmente int(2147483647) em sistemas de 32 bit e int(9223372036854775807) em sistemas de 64 bits.";
 
 //fwite, função para escrever em um arquivo 
 fwrite($arquivo, $curso); 
